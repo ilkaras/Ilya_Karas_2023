9456f5dc37e07a900172afbeca8e97c3cc921bbd
Ilya <karas.ilya@ukr.net>
2023-03-11 14:58:18 +0100

    Lesson1 task3 added

---
+++ Lesson1/Task3/Program2.cs
@@ -0,0 +1,23 @@
+﻿List<int> numbers = new List<int>();
+Console.WriteLine("Write you ten numbers");
+for (int i = 0; i < 10; i++)
+{
+    numbers.Add(Convert.ToInt32(Console.ReadLine()));
+}
+int eleventh;
+Console.WriteLine("type eleventh number");
+eleventh = Convert.ToInt32(Console.ReadLine());
+foreach (int i in numbers)
+{
+    if ( i == eleventh)
+    {
+        numbers.Add(eleventh);
+        break;
+    }
+    else
+        continue;
+}
+foreach (int i in numbers)
+{
+    Console.Write($"{i} ");
+}
