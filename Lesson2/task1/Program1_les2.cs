d43080e2a08d996d678365f31f448e448f8d8e48
Ilya <karas.ilya@ukr.net>
2023-03-18 18:48:26 -0100

    Changed

---
+++ Lesson2/task1/Program1_les2.cs
@@ -0,0 +1,47 @@
+﻿List<Products> things = new List<Products>()
+{
+    new Products("a", 1),
+    new Products("b", 2),
+    new Products("c", 3),
+    new Products("d", 4),
+    new Products("e", 5),
+    new Products("f", 6),
+    new Products("g", 7),
+    new Products("j", 8),
+    new Products("k", 9),
+    new Products("l", 10),
+};
+
+Console.WriteLine("> or <");
+var variant = Console.ReadLine();
+
+if (variant == "<")
+{
+    var sortedproducts = things.OrderBy(p => p.price);
+    foreach (var p in sortedproducts)
+    {
+        Console.WriteLine($" Label ={p.label} price ={p.price}");
+    }
+}
+else if (variant == ">") 
+{
+    var sortedproducts = things.OrderBy(p => p.price).Reverse();
+    foreach (var p in sortedproducts)
+    {
+        Console.WriteLine($" Label ={p.label} price ={p.price}");
+    }
+}
+else {
+Console.WriteLine("Error! Pleas reload the application");
+}
+
+class Products
+{
+    public string label { get; }
+    public int price { get; }
+    public Products(string firstName, int salary)
+    {
+        label = firstName;
+        price = salary;
+    }
+}
