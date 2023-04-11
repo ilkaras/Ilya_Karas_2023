b07b258a4fc0cb03a8e56a7f61803ddb8418bd15
Ilya <karas.ilya@Ukr.net>
2023-04-11 19:25:51 +0200

    Open-Closed

---
+++ Lesson4/OpenClosed/MagicClass.cs
@@ -0,0 +1,27 @@
+﻿using Value;
+
+namespace Open_Closed
+{
+    public class MagicClass
+    {
+        public MagicValue CountYourMagic(int magic)
+        {
+            if (magic == 150)
+            {
+                return new MagicValue
+                {
+                    AmmounPower = magic, Magic = "Fire Wizzzzzard!!!"
+                };
+            }
+
+            if (magic == 50000000)
+            {
+
+                return new MagicValue { AmmounPower = magic, Magic = "Fire Ice Wizzard!!!" };
+
+            }
+
+            return new MagicValue { AmmounPower=magic, Magic="And I didn`t know who are you! ahahah" };
+        }
+    }
+}
---
+++ Lesson4/OpenClosed/MagicValue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Open_Closed;
+
+namespace Value
+{
+    public class MagicValue : Wizard.Magic
+    {
+        public int AmmounPower { get; set; }
+        public string Magic { get; set; }
+        public override void GetInfo()
+        {
+            Console.WriteLine($"You have {AmmounPower} and your magic is {Magic}");
+        }
+    }
+}
