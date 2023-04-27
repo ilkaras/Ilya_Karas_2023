34a5f196c1ace83e93853e885a3914fe865e6d2f
Ilya <karas.ilya@Ukr.net>
2023-04-28 00:38:01 +0200

    Lesson 6 task 1 added

---
+++ Delegats-events-lambda/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Delegates_events_lambda
+{
+    public class Calculator
+    {
+        public delegate float CalculateFunc(float a, float b);
+        public delegate void NotifyHandler(string message);
+        public event NotifyHandler? Notify;
+
+        public float Plus(float a, float b) => a + b;
+        public float Minus(float a, float b) => a - b;
+        public float Multiply(float a, float b) => a * b;
+        public float Divide(float a, float b) => a / b;
+
+        public float Calculate(float a, float b, CalculateFunc operation)
+        {
+            return operation(a, b);
+        }
+
+        public void OnNotify(string message)
+        {
+            Notify?.Invoke(message);
+        }
+    }
+}
---
+++ Delegats-events-lambda/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Delegates_events_lambda
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Calculator calculator = new Calculator();
+            Console.WriteLine("Choose the action: '+','-','*' or '/'");
+            string action = Console.ReadLine();
+            Console.WriteLine("Enter first number");
+            float firstNum = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            float secondNum = float.Parse(Console.ReadLine());
+            calculator.Notify += DisplayMessage;
+            float result;
+
+            switch (action)
+            {
+                case "+":
+                    result = calculator.Calculate(firstNum, secondNum, calculator.Plus);
+                    break;
+
+                case "-":
+                    result = calculator.Calculate(firstNum, secondNum, calculator.Minus);
+                    break;
+
+                case "*":
+                    result = calculator.Calculate(firstNum, secondNum, calculator.Multiply);
+                    break;
+
+                case "/":
+                    result = calculator.Calculate(firstNum, secondNum, calculator.Divide);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid action.");
+                    return;
+            }
+
+            calculator.OnNotify($"Result: {result}");
+        }
+
+        private static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
